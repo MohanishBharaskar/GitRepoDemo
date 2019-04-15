08fc414d86699e4158750890cd75ed0c8f10d79b
Amey Kulkarni <connect@thirstyfish.in>
2019-04-15 16:02:19 +0530

    Map View Controller added

---
+++ GitRepoDemo/MapsForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapsForItinerary.swift
+//  GitRepoDemo
+//
+//  Created by ThirstyFish on 15/04/19.
+//  Copyright © 2019 ThirstyFish. All rights reserved.
+//
+
+import UIKit
+
+class MapsForItinerary: NSObject {
+
+}
